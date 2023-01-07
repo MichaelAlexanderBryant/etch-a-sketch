bedc7741131048ed8b68930089def0a1a6bdc154
MichaelABryant <m.alexander.bryant@gmail.com>
2023-01-07 10:26:49 -0800

    Generate grid

---
+++ eas.js
@@ -0,0 +1,15 @@
+const LENGTH = 16;
+const WIDTH = 16;
+
+const grid = document.getElementById('grid');
+
+function generateGrid() {
+    for (let i=0; i < LENGTH; i++) {
+        grid.innerHTML += `<div class="row" id="${i}"></div>`;
+        for (let j=0; j < WIDTH; j++) {
+            document.getElementById(`${i}`).innerHTML += `<div class="blank-square" id="${i}-${j}"></div>`;
+        };
+    };
+};
+
+generateGrid();
