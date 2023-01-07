8fd784c3ea40d8ac8c09a658b397248afe3d6605
MichaelABryant <m.alexander.bryant@gmail.com>
2023-01-07 13:51:49 -0800

    Fix sketch area resizing issue

--- eas.js
+++ eas.js
@@ -1,19 +1,36 @@
-const LENGTH = 16;
-const WIDTH = 16;
+let length = 16;
+let width = 16;
 
-const grid = document.getElementById('grid');
+const gridContainer = document.getElementById('grid-container');
+let grid = document.getElementById('grid');
 
 function generateGrid() {
-    for (let i=0; i < LENGTH; i++) {
+    for (let i=0; i < length; i++) {
         grid.innerHTML += `<div class="row" id="${i}"></div>`;
-        for (let j=0; j < WIDTH; j++) {
+        for (let j=0; j < width; j++) {
             document.getElementById(`${i}`).innerHTML += `<div class="blank-square" id="${i}-${j}"></div>`;
         };
     };
+    $(".blank-square").on("mouseover", function() {
+        $(this).css("background", "#4C4C4C");
+    });
+    $(".blank-square").css("padding", `${409.6/(length*2)}px`);
 };
 
 generateGrid();
 
-$(".blank-square").on("mouseover", function() {
-    $(this).css("background", "grey");
-});
+function changeSize(){
+    length = prompt("Enter side length (MAX: 100):");
+    while ((+length > 100) || (isNaN(+length))) {
+        alert("Invalid entry");
+        length = prompt("Enter side length (MAX: 100):");
+    }
+    if (length != null) {
+        width = length;
+        grid.remove();
+        grid = document.createElement('div')
+        grid.id = 'grid'
+        gridContainer.appendChild(grid);
+        generateGrid();
+    };
+};
